043d936d4d2501097d527001a5828d36b72eb7b5
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-02-18 11:35:40 +0500

    Aik aur commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
 import WecomePage from "./Pages/WecomePage/index.jsx"
-import CreateARoom from "./Pages/CreateARoom/index.jsx"
 import Meet from "./Pages/Meet/index.jsx"
 import NotFound from "./Pages/NotFound/index.jsx"
 
@@ -11,7 +10,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<WecomePage />} />
-        <Route path="/create-room" element={<CreateARoom />} />
         <Route path="/meet" element={<Meet />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
--- src/Pages/Meet/index.jsx
+++ src/Pages/Meet/index.jsx
@@ -4,7 +4,12 @@ import "./App.css";
 function Meet() {
   return (
     <div className='Meet-Page'>
-      
+      <div className="videoSection">
+        <div>
+          zain
+        </div>
+      </div>
+      <div className="settingSection"></div>
     </div>
   )
 }
