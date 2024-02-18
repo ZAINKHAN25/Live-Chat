95ae5d0323766281692c553bcaa480c3747959c8
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-02-18 20:54:34 +0500

    Setting section done

--- src/Pages/Meet/index.jsx
+++ src/Pages/Meet/index.jsx
@@ -9,7 +9,36 @@ function Meet() {
           zain
         </div>
       </div>
-      <div className="settingSection"></div>
+
+
+      {/* {Setting section} */}
+      <div className="settingSection py-4 px-5 d-flex">
+
+        <div>6:43 PM | yhg-tjbj-ziv</div>
+
+        <div className='iconOfSettings d-flex'>
+
+          <i className="fa-solid redWalaBackground fa-microphone-slash"></i>
+          <i className="fa-solid redWalaBackground fa-video-slash"></i>
+          <i className="fa-solid fa-closed-captioning"></i>
+          <i className="fa-solid fa-face-smile"></i>
+          <i className="fa-solid fa-hand"></i>
+          <i className="fa-solid redWalaBackground fa-phone fa-rotate-by"></i>
+
+        </div>
+
+        <div className='otherIconsOfSettings'>
+          <div className='divOfOtherIconsOfSettings'>
+            <span>1</span>
+            <i className="fa-solid fa-users"></i>
+          </div>
+          <div className='divOfOtherIconsOfSettings'>
+            <span>3</span>
+            <i className="fa-regular fa-message"></i>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
